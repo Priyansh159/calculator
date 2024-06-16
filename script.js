30f7195009930df8c50d25406107ddd8ce03f960
Priyansh159 <154973761+Priyansh159@users.noreply.github.com>
2024-06-16 16:42:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ function clearDisplay(){
     display.value = "";
 }
 
-function calculate(){
-    try {
-        display.value =eval(display.value)
-    }
-    catch(error){
-        display.value ="Error";
+function calculate() {
+    if (display.value === "159") {
+        display.value = "Priyansh Rana";
+    } else {
+        try {
+            display.value = eval(display.value);
+        } catch (error) {
+            display.value = "Error";
+        }
     }
 }
